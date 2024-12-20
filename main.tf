bfac277bd8e62a0f492f6a43361bea484c694ec3
Shreya1shree5 <47668807+Shreya1shree5@users.noreply.github.com>
2024-12-20 17:39:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,61 +1,46 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-provider "google-beta" {
-  project = var.project_id
-  region  = var.region
-}
-
 resource "google_container_cluster" "minimal" {
   name               = "minimal-gke-cluster"
   location           = var.region
   initial_node_count = 1
-
-  # Remove default node pool to create a custom one with minimal resources
   remove_default_node_pool = true
-
-  # Network settings
-  network    = var.network
-  subnetwork = var.subnetwork
+  network            = var.network
+  subnetwork         = var.subnetwork
 }
 
 resource "google_container_node_pool" "minimal_nodes" {
-  cluster  = google_container_cluster.minimal.name
-  location = google_container_cluster.minimal.location
-  node_count = 1 # Minimum nodes in the pool
+  cluster    = google_container_cluster.minimal.name
+  location   = google_container_cluster.minimal.location
+  node_count = 1
 
   node_config {
-    machine_type = "e2-small" # Smallest available machine type
-    disk_type    = "pd-standard"
-    disk_size_gb = 10 # Minimal disk size
+    machine_type = "e2-small"
+    disk_type    = "pd-standard" # Avoid SSD quota consumption
+    disk_size_gb = 10
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
 }
 
-# Variables
 variable "project_id" {
-  description = "The Google Cloud project ID."
   type        = string
+  description = "The Google Cloud project ID."
 }
 
 variable "region" {
-  description = "The region to deploy the GKE cluster in."
   type        = string
-  default     = "us-central1"
+  description = "The region to deploy the GKE cluster in."
+  default     = "us-east1" # Change region if quota is insufficient
 }
 
 variable "network" {
-  description = "The VPC network to use for the GKE cluster."
   type        = string
+  description = "The VPC network to use for the GKE cluster."
   default     = "default"
 }
 
 variable "subnetwork" {
-  description = "The subnetwork to use for the GKE cluster."
   type        = string
+  description = "The subnetwork to use for the GKE cluster."
   default     = "default"
 }
